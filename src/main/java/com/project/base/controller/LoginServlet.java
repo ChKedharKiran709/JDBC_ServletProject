179a71cc1f6d26c012cff2f1c31e46822ff6e379
ChKedharKiran709 <98611136+ChKedharKiran709@users.noreply.github.com>
2024-12-26 14:09:46 +0530

    added Login and Logout features

---
+++ src/main/java/com/project/base/controller/LoginServlet.java
@@ -0,0 +1,27 @@
+package com.project.base.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        // Hardcoded credentials for testing
+        if ("kedhar".equals(username) && "kedhar".equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("ProjectWelcome.html");
+        } else {
+            response.sendRedirect("Login.html?error=invalid");
+        }
+    }
+}
